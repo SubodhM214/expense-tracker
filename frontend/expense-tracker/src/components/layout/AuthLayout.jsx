767b47afea0d396eac1d99d036ef785ae514f0c0
Subodh2312 <159807214+SubodhM214@users.noreply.github.com>
2025-08-02 18:32:47 +0530

    Fixed icon issue and updated AuthLayout component

--- frontend/expense-tracker/src/components/layout/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layout/AuthLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CARD_2 from "../../assets/images/card2.png";
 import { LuTrendingUpDown } from "react-icons/lu";
+
 const AuthLayout = ({ children }) => {
   return (
     <div className="flex">
@@ -35,7 +36,9 @@ export default AuthLayout;
 const StatsInfoCard = ({ icon, label, values, color }) => {
   return (
     <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border-gray-200/50 z-10 ">
-      <div className="w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl">
+      <div
+        className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}
+      >
         {icon}
       </div>
       <div>
