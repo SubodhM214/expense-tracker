1171b4786436b6dc7b9b8bf30dbf754d2a509a46
Subodh2312 <159807214+SubodhM214@users.noreply.github.com>
2025-09-25 10:25:19 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*" || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "*",
     METHODS: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   })
 );
 app.use(express.json());
